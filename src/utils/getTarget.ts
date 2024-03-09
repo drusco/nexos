96bbc09746a6eb2655909ee88c585b2de39b4b93
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-03-09 20:16:57 -0300

    docs: comments for the getTarget function

--- src/utils/getTarget.ts
+++ src/utils/getTarget.ts
@@ -1,20 +1,23 @@
 import findProxy from "./findProxy.js";
 import map from "./map.js";
 
-const getTarget = (value: unknown, getProxy: boolean = false): unknown => {
+const getTarget = (value: unknown, resolveProxy: boolean = false): unknown => {
   const proxy = findProxy(value);
 
   if (!proxy) {
+    // returns a value that is not linked to a proxy in any way
     return value;
   }
 
-  if (proxy && getProxy) {
+  if (proxy && resolveProxy) {
+    // returns a proxy that is linked to the received value
     return proxy;
   }
 
   const { target } = map.proxies.get(proxy);
 
   if (target) {
+    // returns a target from a proxy that is linked to the received value
     return target.deref();
   }
 };
