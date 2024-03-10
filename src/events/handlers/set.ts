faa3ee5a9872f5ea3eea0760dd08fa3905fdf0c1
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-03-10 20:37:02 -0300

    feat: adds proxyevent to set handler

--- src/events/handlers/set.ts
+++ src/events/handlers/set.ts
@@ -1,23 +1,31 @@
 import Nexo from "../../types/Nexo.js";
 import { getTarget, isTraceable, map } from "../../utils/index.js";
+import ProxyEvent from "../ProxyEvent.js";
 
 const set = (mock: Nexo.Mock, key: Nexo.objectKey, value: unknown): boolean => {
   const proxy = map.tracables.get(mock);
-  const { sandbox } = map.proxies.get(proxy);
+  const data = map.proxies.get(proxy);
+  const { sandbox } = data;
+  const scope = data.scope.deref();
 
-  // const origin: Nexo.proxy.origin.set = {
-  //   name: "set",
-  //   proxy,
-  //   key,
-  //   value,
-  // };
+  let _value = getTarget(value, true);
 
-  const target = getTarget(value, true);
+  const event = new ProxyEvent("handler.set", {
+    proxy,
+    key,
+    value,
+  });
 
-  if (isTraceable(target)) {
-    sandbox.set(key, new WeakRef(target));
+  scope.emit(event.name, event);
+
+  if (event.defaultPrevented) {
+    _value = event.returnValue;
+  }
+
+  if (isTraceable(_value)) {
+    sandbox.set(key, new WeakRef(_value));
   } else {
-    sandbox.set(key, target);
+    sandbox.set(key, _value);
   }
 
   return true;
