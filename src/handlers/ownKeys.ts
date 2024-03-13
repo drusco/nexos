ba745da86814c5c5ffd213f53f82c240f6bb626c
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-03-13 03:24:18 -0300

    feat: adds ownKey handler

---
+++ src/handlers/ownKeys.ts
@@ -0,0 +1,32 @@
+import Nexo from "../types/Nexo.js";
+import { map } from "../utils/index.js";
+import ProxyHandlerEvent from "../events/ProxyHandlerEvent.js";
+
+const ownKeys = (mock: Nexo.Mock): Nexo.objectKey[] => {
+  const proxy = map.tracables.get(mock);
+  const data = map.proxies.get(proxy);
+  const { sandbox } = data;
+  const scope = data.scope.deref();
+  const keys: Nexo.objectKey[] = [];
+
+  const event = new ProxyHandlerEvent("isExtensible", proxy);
+
+  scope.emit(event.name, event);
+
+  if (event.defaultPrevented) {
+    if (!Array.isArray(event.returnValue)) {
+      return keys;
+    }
+    return event.returnValue.filter(
+      (key) => typeof key === "string" || typeof key === "symbol",
+    );
+  }
+
+  for (const key of sandbox.keys()) {
+    keys.push(key);
+  }
+
+  return keys;
+};
+
+export default ownKeys;
