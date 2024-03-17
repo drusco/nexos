ed92f05adb921982d575d89afde056e7efa82d28
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-03-16 21:54:37 -0300

    fix: improve encode function

--- src/utils/encode.ts
+++ src/utils/encode.ts
@@ -1,5 +1,6 @@
 import Nexo from "../types/Nexo.js";
 import { findProxy, isTraceable, getProxyPayload } from "./index.js";
+import isPlainObject from "is-plain-obj";
 
 // Encodes a proxy to its string (payload) representation
 
@@ -9,26 +10,38 @@ export default function encode<Type>(
 ): Type | string {
   const proxy = findProxy(value);
 
+  // return the string that represents the proxy
+
   if (proxy) {
     return getProxyPayload(proxy);
   }
+
+  // return original value for untraceable values
 
   if (!isTraceable(value)) {
     return value;
   }
 
-  if (typeof value === "function") {
-    return value;
-  }
+  // Handle circular reference by returning the original value
 
   if (visited.has(value)) {
-    // Handle circular reference by returning the original value
     return value;
   }
 
   visited.add(value);
 
-  const copy = (Array.isArray(value) ? [] : {}) as Type;
+  // return original value
+
+  const isObject = isPlainObject(value);
+  const isArray = Array.isArray(value);
+
+  if (!(isObject || isArray)) {
+    return value;
+  }
+
+  // return shallow copy for plain-objects and arrays
+
+  const copy = (isArray ? [] : {}) as Type;
   const keys = Object.keys(value);
 
   keys.forEach((key) => {
