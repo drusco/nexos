44d4b2ac29674de563a4f008cc702c490be02b9a
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-03-19 13:51:45 -0300

    test: add proxyIterator util test

---
+++ src/utils/proxyIterator.test.ts
@@ -0,0 +1,38 @@
+import Nexo from "../Nexo.js";
+import NexoTS from "../types/Nexo.js";
+import { proxyIterator, map } from "./index.js";
+
+const nexo = new Nexo();
+
+describe("proxyIterator", () => {
+  it("Returns an iterable iterator of proxies", () => {
+    const foo = nexo.create();
+    const bar = nexo.create();
+
+    const iterator = proxyIterator(nexo);
+    const proxies = [...iterator];
+
+    expect(proxies.length).toBe(2);
+    expect(proxies.includes(foo)).toBe(true);
+    expect(proxies.includes(bar)).toBe(true);
+  });
+
+  it("Emits a deletion event when the proxy does not exists anymore", () => {
+    const proxy = nexo.create();
+    const { id } = map.proxies.get(proxy);
+    const callback = jest.fn();
+
+    const weakRefMock = {
+      deref() {},
+    } as WeakRef<NexoTS.Proxy>;
+
+    nexo.proxies.set(id, weakRefMock);
+    nexo.addListener("nx.delete", callback);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const proxies = [...proxyIterator(nexo)];
+
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith(id);
+  });
+});
