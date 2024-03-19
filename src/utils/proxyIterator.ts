928a45e562da28da2d6afa438520a7448b5613a1
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-03-19 11:19:51 -0300

    feat: remove proxy id from map when proxy is unavailable while using iterator

--- src/utils/proxyIterator.ts
+++ src/utils/proxyIterator.ts
@@ -1,9 +1,10 @@
 import Nexo from "../types/Nexo.js";
 
 const proxyIterator = function* (scope: Nexo): IterableIterator<Nexo.Proxy> {
-  for (const [, ref] of scope.proxies) {
+  for (const [id, ref] of scope.proxies) {
     const proxy = ref.deref();
     if (proxy) yield proxy;
+    else scope.proxies.delete(id);
   }
 };
 
