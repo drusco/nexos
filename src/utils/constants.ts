a8c640a50360637a65fc180095dfbd144a067773
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-03-19 12:03:12 -0300

    fix: update constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,17 @@
 const NO_BREAK: string = "⁠";
-const IS_PROXY_ID_REGEXP: RegExp = new RegExp(`^${NO_BREAK}\\d+$`);
-const HAS_PROXY_ID_REGEXP: RegExp = new RegExp(`(${NO_BREAK}\\d+)`, "g");
+
+const UUID_REGEXP =
+  "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
+
+const IS_PROXY_ID_REGEXP: RegExp = new RegExp(
+  `^${NO_BREAK + UUID_REGEXP}$`,
+  "i",
+);
+
+const HAS_PROXY_ID_REGEXP: RegExp = new RegExp(
+  `(${NO_BREAK + UUID_REGEXP})`,
+  "gi",
+);
 
 export default {
   IS_PROXY_ID_REGEXP,
