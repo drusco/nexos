552a17767d04e1e407432b09b98b2cef4c0a6791
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-04-02 22:17:05 -0300

    test: add tests for apply handler

--- src/utils/handlers/apply.test.ts
+++ src/utils/handlers/apply.test.ts
@@ -1,22 +1,22 @@
 import Nexo from "../../lib/Nexo.js";
 import NexoEvent from "../../lib/events/NexoEvent.js";
+import isProxy from "../isProxy.js";
 import apply from "./apply.js";
-
-const nexo = new Nexo();
 
 describe("apply", () => {
   it("Emits an event to the nexo class and to the proxy", () => {
+    const nexo = new Nexo();
     const proxy = nexo.proxy();
     const wrapper = Nexo.wrap(proxy);
     const args = ["foo", 123, { test: true }];
-    const $this = {};
+    const _this = {};
     const applyCallbackNexo = jest.fn();
     const applyCallbackProxy = jest.fn();
 
     nexo.on("nx.proxy.apply", applyCallbackNexo);
     wrapper.on("nx.proxy.apply", applyCallbackProxy);
 
-    apply(wrapper, $this, args);
+    apply(wrapper, _this, args);
 
     const [applyEventForNexo] = applyCallbackNexo.mock.lastCall;
     const [applyEventForProxy] = applyCallbackProxy.mock.lastCall;
@@ -24,18 +24,21 @@ describe("apply", () => {
     expect(applyCallbackNexo).toBeCalledTimes(1);
     expect(applyEventForNexo.target).toBe(proxy);
     expect(applyEventForNexo.cancellable).toBe(true);
-    expect(applyEventForNexo.data.arguments).toBe(args);
-    expect(applyEventForNexo.data.this).toBe($this);
+    expect(applyEventForNexo.data).toStrictEqual({
+      this: _this,
+      arguments: args,
+    });
 
     expect(applyCallbackProxy).toBeCalledTimes(1);
     expect(applyEventForProxy).toBe(applyEventForNexo);
   });
 
   it("Allows its return value to be defined by the event listener", () => {
+    const nexo = new Nexo();
     const proxy = nexo.proxy();
     const wrapper = Nexo.wrap(proxy);
-    const args = ["foo", 123, { test: true }];
-    const $this = {};
+    const args = ["foo", "bar", 100];
+    const _this = undefined;
     const expectedResult = [];
 
     nexo.on("nx.proxy.apply", (event: NexoEvent) => {
@@ -43,8 +46,33 @@ describe("apply", () => {
       event.returnValue = expectedResult;
     });
 
-    const result = apply(wrapper, $this, args);
+    const result = apply(wrapper, _this, args);
 
     expect(result).toBe(expectedResult);
+  });
+
+  it("Allows its return value to be defined by a function target", () => {
+    const nexo = new Nexo();
+    const target = (a: number, b: number): number => a + b;
+    const args = [4, 1];
+    const _this = undefined;
+    const proxy = nexo.proxy(target);
+    const wrapper = Nexo.wrap(proxy);
+
+    const result = apply(wrapper, _this, args);
+
+    expect(result).toBe(5);
+  });
+
+  it("Returns a new proxy by default", () => {
+    const nexo = new Nexo();
+    const proxy = nexo.proxy();
+    const args = [];
+    const _this = undefined;
+    const wrapper = Nexo.wrap(proxy);
+
+    const result = apply(wrapper, _this, args);
+
+    expect(isProxy(result)).toBe(true);
   });
 });
