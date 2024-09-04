b86a91247cfcee573d0002e793ee7bddc099fd06
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-09-04 04:10:17 -0300

    feat: add preventExtensions test

---
+++ src/handlers/preventExtensions.test.ts
@@ -0,0 +1,32 @@
+import ProxyEvent from "../events/ProxyEvent.js";
+import Nexo from "../Nexo.js";
+import ProxyWrapper from "../utils/ProxyWrapper.js";
+
+describe("preventExtensions", () => {
+  it("Returns true when the preventExtension operation succeeds", () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create();
+    const result = Reflect.preventExtensions(proxy);
+
+    expect(result).toBe(true);
+  });
+
+  it("Emits an preventExtensions event", () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create();
+    const wrapper = Nexo.wrap(proxy) as ProxyWrapper;
+    const listener = jest.fn();
+
+    wrapper.on("proxy.preventExtensions", listener);
+
+    const result = Reflect.preventExtensions(proxy);
+
+    const [event]: [ProxyEvent] = listener.mock.lastCall;
+
+    expect(listener).toHaveBeenCalledTimes(1);
+    expect(event.target).toBe(proxy);
+    expect(event.cancelable).toBe(false);
+    expect(event.data).toBeUndefined();
+    expect(result).toBe(true);
+  });
+});
