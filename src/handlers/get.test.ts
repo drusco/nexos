f2a50e91c1433052af0291228e1d2e4d6a401d72
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-09-07 22:32:56 -0300

    feat: add set handler and its tests

---
+++ src/handlers/get.test.ts
@@ -0,0 +1,48 @@
+import type nx from "../types/Nexo.js";
+import Nexo from "../Nexo.js";
+import ProxyEvent from "../events/ProxyEvent.js";
+import { isProxy } from "util/types";
+
+describe("get", () => {
+  it("Emits an event with custom data", () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create();
+    const wrapper = Nexo.wrap(proxy);
+    const listener = jest.fn();
+
+    nexo.on("proxy.get", listener);
+    wrapper.on("proxy.get", listener);
+
+    proxy.name = "foo";
+
+    const result = proxy.name;
+
+    const [event]: [ProxyEvent<{ property: nx.objectKey; result: unknown }>] =
+      listener.mock.lastCall;
+
+    expect(listener).toHaveBeenCalledTimes(2);
+    expect(event).toBeInstanceOf(ProxyEvent);
+    expect(event.target).toBe(proxy);
+    expect(event.cancelable).toBe(false);
+    expect(event.data.property).toBe("name");
+    expect(event.data.result).toBe(result);
+    expect(proxy.name).toBe("foo");
+  });
+
+  it("Returns a value from the proxy target", () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create({ foo: true });
+    const result = proxy.foo;
+
+    expect(result).toBe(true);
+  });
+
+  it("Returns a new proxy when the property does not exists on the sandbox", () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create();
+    const result = proxy.foo;
+
+    expect(result).not.toBeUndefined();
+    expect(isProxy(result)).toBe(true);
+  });
+});
