98e963547559545a79b2e4a9dbc98cb68a691043
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-03-30 04:52:26 -0300

    docs(docusaurus): updates docs confug files

--- docusaurus/plugins/typedoc-plugin-hooks.mjs
+++ docusaurus/plugins/typedoc-plugin-hooks.mjs
@@ -1,12 +1,15 @@
 // ** PLugin to customize the markdown files
-import { projectName } from "@site/docusaurus.config";
+
 // @ts-check
 import { MarkdownPageEvent } from "typedoc-plugin-markdown";
+import docusaurusConfig from "../docusaurus.json" with { type: "json" };
+
 /**
  * @param {import('typedoc-plugin-markdown').MarkdownApplication} app
  */
 export function load(app) {
   app.renderer.on(MarkdownPageEvent.END, (page) => {
+    const { projectName } = docusaurusConfig;
     switch (page.url) {
       case "index.md":
         page.contents = page.contents.replace(
