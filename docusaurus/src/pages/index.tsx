8851dceeaa017378fa6b285b74fad25f12785532
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-03-30 00:28:10 -0300

    docs(docusaurus): updates the nexos docs

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -3,15 +3,19 @@ import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Link from "@docusaurus/Link";
 
+type customFields = {
+  [x: string]: string;
+};
+
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
-  const { lastVersion } = siteConfig.customFields;
+  const { lastVersion, projectName } = siteConfig.customFields as customFields;
   const versionPath = lastVersion === "current" ? "" : lastVersion;
 
   return (
     <Layout title="Welcome!">
       <div style={{ textAlign: "center", padding: "2rem" }}>
-        <h1>nexos</h1>
+        <h1>{projectName}</h1>
         <p>Simplifies proxy creation and trap handling using events</p>
         <Link to={`/docs/${versionPath}`}>Get started</Link>
       </div>
---
+++ docusaurus/utils/wordsUpperCase.ts
@@ -0,0 +1,9 @@
+const wordsUpperCase = (text: string): string =>
+  text
+    .split(" ")
+    .map((word) =>
+      word.length < 3 ? word : word[0].toUpperCase() + word.slice(1),
+    )
+    .join(" ");
+
+export default wordsUpperCase;
