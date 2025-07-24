02d4ed4495684ae51bb294b2fc7ee6dd829cdc5c
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-07-24 16:41:06 -0300

    feat(deferred): adds an util that creates a promise and exposes its deferred functions

---
+++ src/utils/createDeferred.ts
@@ -0,0 +1,11 @@
+export default function createDeferred<T>() {
+  let resolve!: (value: T | Promise<T>) => void;
+  let reject!: (reason?: Error) => void;
+
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  return { promise, resolve, reject };
+}
