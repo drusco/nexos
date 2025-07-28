5cc4b32654a38095a11a8cc997641798fba1a3e9
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-07-28 02:22:17 -0300

    feat(proxy.get): allows the proxy.get event to be cancelable and to return a different value

--- src/handlers/get.test.ts
+++ src/handlers/get.test.ts
@@ -2,32 +2,62 @@ import type * as nx from "../types/Nexo.js";
 import Nexo from "../Nexo.js";
 import ProxyEvent from "../events/ProxyEvent.js";
 
-describe("get", () => {
-  it("Emits an event with custom data", () => {
+describe("Get Handler", () => {
+  it("emits an event with custom data", async () => {
     const nexo = new Nexo();
     const proxy = nexo.create();
     const wrapper = Nexo.wrap(proxy);
     const listener = jest.fn();
+    const property = "name";
+    const value = "foo";
 
     nexo.on("proxy.get", listener);
     wrapper.on("proxy.get", listener);
 
-    proxy.name = "foo";
+    proxy[property] = value;
 
-    const result = proxy.name;
-
+    const result = proxy[property];
     const [event]: [nx.ProxyGetEvent] = listener.mock.lastCall;
+    const getResultFn: nx.FunctionLike = await event.data.result;
 
     expect(listener).toHaveBeenCalledTimes(2);
     expect(event).toBeInstanceOf(ProxyEvent);
     expect(event.target).toBe(proxy);
-    expect(event.cancelable).toBe(false);
-    expect(event.data.property).toBe("name");
-    expect(event.data.result).toBe(result);
-    expect(proxy.name).toBe("foo");
+    expect(event.cancelable).toBe(true);
+    expect(event.data.property).toBe(property);
+    expect(event.data.result).toBeInstanceOf(Promise);
+    expect(getResultFn()).toBe(result);
+    expect(proxy[property]).toBe(value);
   });
 
-  it("Returns a value from the proxy target", () => {
+  it("prevents the default behavior and returns a different value", async () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create();
+    const wrapper = Nexo.wrap(proxy);
+
+    const property = "name";
+    const value = "baz";
+    const newValue = "foo";
+
+    const listener = jest.fn((event: nx.ProxyGetEvent) => {
+      event.preventDefault();
+      return newValue;
+    });
+
+    wrapper.on("proxy.get", listener);
+
+    proxy[property] = value;
+
+    const result = proxy[property];
+    const [event]: [nx.ProxyGetEvent] = listener.mock.lastCall;
+    const getResultFn: nx.FunctionLike = await event.data.result;
+
+    expect(result).not.toBe(value);
+    expect(result).toBe(newValue);
+    expect(getResultFn()).toBe(newValue);
+  });
+
+  it("returns a value from the proxy target", () => {
     const nexo = new Nexo();
     const proxy = nexo.create({ foo: true });
     const result = proxy.foo;
@@ -35,7 +65,7 @@ describe("get", () => {
     expect(result).toBe(true);
   });
 
-  it("Returns a new proxy when the property does not exists on the sandbox", () => {
+  it("returns a new proxy when the property does not exists on the sandbox", () => {
     const nexo = new Nexo();
     const proxy = nexo.create();
     const result = proxy.foo;
