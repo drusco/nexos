0db6a65581ac4e3c244c2f862524da27fe1a31c2
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-07-30 19:10:45 -0300

    feat(proxy.has): adds support for cancelable events on the proxy.has handler

--- src/handlers/has.ts
+++ src/handlers/has.ts
@@ -1,28 +1,43 @@
 import type * as nx from "../types/Nexo.js";
 import ProxyEvent from "../events/ProxyEvent.js";
+import { createDeferred, rejectWith, resolveWith } from "../utils/deferred.js";
+import ProxyError from "../errors/ProxyError.js";
 
 export default function has(resolveProxy: nx.resolveProxy) {
   return (target: nx.Traceable, property: nx.ObjectKey): boolean => {
     const [proxy, wrapper] = resolveProxy();
     const { sandbox } = wrapper;
-    const targetHasProperty = Reflect.has(target, property);
-    const sandboxHasProperty = sandbox ? Reflect.has(sandbox, property) : false;
-    const result = sandbox ? sandboxHasProperty : targetHasProperty;
+    const deferred = createDeferred<nx.FunctionLike<[], boolean>>();
 
-    new ProxyEvent("has", {
+    const event = new ProxyEvent<nx.ProxyHasEvent["data"]>("has", {
       target: proxy,
-      cancelable: false,
+      cancelable: true,
       data: {
         target: sandbox || target,
         property,
-        result,
+        result: deferred.promise,
       },
-    });
+    }) as nx.ProxyHasEvent;
 
-    if (sandbox) {
-      return sandboxHasProperty;
+    if (event.defaultPrevented) {
+      try {
+        const returnValue = event.returnValue;
+        if (typeof returnValue !== "boolean") {
+          throw new TypeError(`'has' trap must return a boolean value`);
+        }
+        return resolveWith(deferred.resolve, returnValue);
+      } catch (error) {
+        return rejectWith(
+          deferred.resolve,
+          new ProxyError(error.message, proxy),
+        );
+      }
     }
 
-    return targetHasProperty;
+    if (sandbox) {
+      return resolveWith(deferred.resolve, Reflect.has(sandbox, property));
+    }
+
+    return resolveWith(deferred.resolve, Reflect.has(target, property));
   };
 }
