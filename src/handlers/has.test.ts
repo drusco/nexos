bc5c9f9052502225b5c94881cf893556eb174d5f
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-07-30 19:10:45 -0300

    feat(proxy.has): adds support for cancelable events on the proxy.has handler

--- src/handlers/has.test.ts
+++ src/handlers/has.test.ts
@@ -1,9 +1,10 @@
 import type * as nx from "../types/Nexo.js";
 import ProxyEvent from "../events/ProxyEvent.js";
 import Nexo from "../Nexo.js";
+import ProxyError from "../errors/ProxyError.js";
 
-describe("has", () => {
-  it("Emits an event with custom data", () => {
+describe("Has Handler", () => {
+  it("emits an event with custom data", async () => {
     const nexo = new Nexo();
     const proxy = nexo.create();
     const wrapper = Nexo.wrap(proxy);
@@ -15,16 +16,51 @@ describe("has", () => {
     Reflect.has(proxy, "foo");
 
     const [event]: [nx.ProxyHasEvent] = listener.mock.lastCall;
+    const getResultFn: nx.FunctionLike = await event.data.result;
 
     expect(listener).toHaveBeenCalledTimes(2);
     expect(event).toBeInstanceOf(ProxyEvent);
-    expect(event.cancelable).toBe(false);
+    expect(event.cancelable).toBe(true);
     expect(event.target).toBe(proxy);
     expect(event.data.property).toBe("foo");
-    expect(event.data.result).toBe(false);
+    expect(getResultFn()).toBe(false);
   });
 
-  it("Returns a boolean indiating whether the property exists in the target", () => {
+  it("prevents the default has trap return value", async () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create();
+    const wrapper = Nexo.wrap(proxy);
+
+    const listener = jest.fn((event: nx.ProxyHasEvent) => {
+      event.preventDefault();
+      return true;
+    });
+
+    wrapper.on("proxy.has", listener);
+
+    const result = Reflect.has(proxy, "foo");
+
+    const [event]: [nx.ProxyHasEvent] = listener.mock.lastCall;
+    const getResultFn: nx.FunctionLike = await event.data.result;
+
+    expect(result).toBe(true);
+    expect(getResultFn()).toBe(true);
+  });
+
+  it("throws an error when the return value is not a boolean", async () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create();
+    const wrapper = Nexo.wrap(proxy);
+
+    wrapper.on("proxy.has", (event: nx.ProxyHasEvent) => {
+      event.preventDefault();
+      return "invalid";
+    });
+
+    expect(() => Reflect.has(proxy, "foo")).toThrow(ProxyError);
+  });
+
+  it("returns a boolean indiating whether the property exists in the target", () => {
     const nexo = new Nexo();
     const target = {};
     const proxy = nexo.create(target);
@@ -33,7 +69,7 @@ describe("has", () => {
     expect(Reflect.has(proxy, "foo")).toBe(false);
   });
 
-  it("Returns a boolean indiating whether the property exists in the sandbox", () => {
+  it("returns a boolean indiating whether the property exists in the sandbox", () => {
     const nexo = new Nexo();
     const proxy = nexo.create();
     const fakeArray = nexo.create();
