47165732091a2ba1599676672fae641223d8f513
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-08-01 21:51:58 -0300

    feat(prevent.extensions): adds full support on proxies to prevent the proxy.preventExtensions event

--- src/handlers/preventExtensions.test.ts
+++ src/handlers/preventExtensions.test.ts
@@ -1,16 +1,31 @@
 import type * as nx from "../types/Nexo.js";
 import Nexo from "../Nexo.js";
+import ProxyError from "../errors/ProxyError.js";
 
-describe("preventExtensions", () => {
-  it("Returns true when the preventExtension operation succeeds", () => {
+describe("PreventExtensions Handler", () => {
+  it("prevents extensions on the sandboxed proxies", () => {
     const nexo = new Nexo();
     const proxy = nexo.create();
+    const wrapper = Nexo.wrap(proxy);
     const result = Reflect.preventExtensions(proxy);
 
     expect(result).toBe(true);
+    expect(Object.isExtensible(proxy)).toBe(false);
+    expect(Object.isExtensible(wrapper.sandbox)).toBe(false);
   });
 
-  it("Emits an preventExtensions event", () => {
+  it("prevents extensions on the proxy target", () => {
+    const nexo = new Nexo();
+    const target = {};
+    const proxy = nexo.create(target);
+    const result = Reflect.preventExtensions(proxy);
+
+    expect(result).toBe(true);
+    expect(Object.isExtensible(proxy)).toBe(false);
+    expect(Object.isExtensible(target)).toBe(false);
+  });
+
+  it("emits a 'proxy.preventExtensions' event", async () => {
     const nexo = new Nexo();
     const proxy = nexo.create();
     const wrapper = Nexo.wrap(proxy);
@@ -22,11 +37,51 @@ describe("preventExtensions", () => {
     const result = Reflect.preventExtensions(proxy);
 
     const [event]: [nx.ProxyPreventExtensionsEvent] = listener.mock.lastCall;
+    const getResult: nx.FunctionLike = await event.data.result;
 
     expect(listener).toHaveBeenCalledTimes(2);
+    expect(event.name).toBe("proxy.preventExtensions");
     expect(event.target).toBe(proxy);
-    expect(event.cancelable).toBe(false);
-    expect(event.data.result).toBe(true);
+    expect(event.cancelable).toBe(true);
+    expect(getResult()).toBe(true);
     expect(result).toBe(true);
+  });
+
+  it("prevents the default behavior on the operation", async () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create();
+    const wrapper = Nexo.wrap(proxy);
+
+    const listener = jest.fn((event: nx.ProxyPreventExtensionsEvent) => {
+      event.preventDefault();
+      return false;
+    });
+
+    wrapper.on("proxy.preventExtensions", listener);
+
+    const result = Reflect.preventExtensions(proxy);
+    const [event]: [nx.ProxyPreventExtensionsEvent] = listener.mock.lastCall;
+    const getResult: nx.FunctionLike = await event.data.result;
+
+    expect(result).toBe(false);
+    expect(getResult()).toBe(false);
+    expect(event.returnValue).toBe(false);
+    expect(Reflect.isExtensible(proxy)).toBe(true);
+  });
+
+  it("throws if the return value is not false or undefined", () => {
+    const nexo = new Nexo();
+    const proxy = nexo.create();
+    const wrapper = Nexo.wrap(proxy);
+
+    const listener = jest.fn((event: nx.ProxyPreventExtensionsEvent) => {
+      event.preventDefault();
+      return true;
+    });
+
+    wrapper.on("proxy.preventExtensions", listener);
+
+    expect(() => Reflect.preventExtensions(proxy)).toThrow(ProxyError);
+    expect(Reflect.isExtensible(proxy)).toBe(true);
   });
 });
