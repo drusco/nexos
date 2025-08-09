7afbee669de870eedb5371e92f600a73a3cfc52d
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-08-09 12:44:54 -0300

    docs(docusaurus): removes the docs from the repo

--- docusaurus/utils/words-uppercase.ts
+++
@@ -1,9 +0,0 @@
-const wordsUpperCase = (text: string): string =>
-  text
-    .split(/\W+/gm)
-    .map((word) =>
-      word.length < 3 ? word : word[0].toUpperCase() + word.slice(1),
-    )
-    .join(" ");
-
-export default wordsUpperCase;
