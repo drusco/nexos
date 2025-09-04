e8e04541bcdae5d826e35976f056d69bffdfbafe
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-09-04 01:00:38 -0300

    fix(get.proxy.wrapper): update the getProxyWrapper tests

--- src/utils/getProxyWrapper.test.ts
+++ src/utils/getProxyWrapper.test.ts
@@ -1,34 +1,36 @@
 import Nexo from "../Nexo.js";
+import getProxyWrapper from "./getProxyWrapper.js";
 import maps from "./maps.js";
 import ProxyError from "./ProxyError.js";
 import ProxyWrapper from "./ProxyWrapper.js";
 
 describe("getProxyWrapper", () => {
-  it("Access the wrapper using the sanboxed proxy", () => {
+  it("Access the wrapper using a sanboxed proxy", () => {
     const nexo = new Nexo();
     const proxy = nexo.create();
-    const wrapper = Nexo.wrap(proxy);
+    const wrapper = getProxyWrapper(proxy);
 
     expect(wrapper).toBeInstanceOf(ProxyWrapper);
+    expect(wrapper.traceable).toBe(false);
   });
 
-  it("Access the wrapper using the traceable proxy", () => {
+  it("Access the wrapper using a traceable proxy", () => {
     const nexo = new Nexo();
     const target = {};
     const proxy = nexo.create(target);
-    const wrapper = Nexo.wrap(proxy);
+    const wrapper = getProxyWrapper(proxy);
 
     expect(wrapper).toBeInstanceOf(ProxyWrapper);
+    expect(wrapper.traceable).toBe(true);
   });
 
   it("Throws when the wrapper cannot be found", () => {
     const nexo = new Nexo();
-    const target = {};
-    const proxy = nexo.create(target);
+    const proxy = nexo.create();
 
     // force proxy removal from map of proxies
     maps.proxies.delete(proxy);
 
-    expect(() => Nexo.wrap(proxy)).toThrow(ProxyError);
+    expect(() => getProxyWrapper(proxy)).toThrow(ProxyError);
   });
 });
