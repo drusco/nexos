272169e1a21bbc6958793693f79093a5bb9d7b19
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-09-16 00:53:20 -0300

    feat(emitproxy): move the proxy event emission to an external utility

--- src/utils/createProxy.ts
+++ src/utils/createProxy.ts
@@ -1,9 +1,7 @@
 import type * as nx from "../types/Nexo.js";
 import getProxyMap from "./getProxyMap.js";
-import ProxyCreateEvent from "../events/ProxyCreateEvent.js";
-import { createDeferred, resolveWith } from "./deferred.js";
-import isProxy from "./isProxy.js";
 import getProxy from "./getProxy.js";
+import emitProxy from "./emitProxy.js";
 
 const createProxy = (
   nexo: nx.Nexo,
@@ -33,41 +31,7 @@ const createProxy = (
 
   // create and emit a 'proxy' event to the event listeners
 
-  const deferred = createDeferred<nx.FunctionLike<[], nx.Proxy>>();
-
-  const event = new ProxyCreateEvent({
-    target: proxy,
-    data: {
-      id: wrapper.id,
-      target: wrapper.target,
-      result: deferred.promise,
-    },
-  });
-
-  // check whether the event got prevented
-  if (event.defaultPrevented) {
-    const { returnValue } = event;
-
-    if (isProxy(returnValue) && returnValue !== proxy) {
-      // get the new proxy wrapper
-      const returnedProxyWrapper = proxyMap.get(returnValue);
-      // revoke the original proxy in the event
-      wrapper.revoke();
-      // remove the original proxy from the maps
-      proxyMap.delete(proxy);
-      returnedProxyWrapper?.nexo?.entries.delete(wrapper.id);
-      // add or update the ID to the returned proxy
-      returnedProxyWrapper?.nexo?.entries.set(
-        proxyMap.get(returnValue).id,
-        new WeakRef(returnValue),
-      );
-
-      // return a different proxy object
-      return resolveWith(deferred.resolve, returnValue);
-    }
-  }
-
-  return resolveWith(deferred.resolve, proxy);
+  return emitProxy(proxy);
 };
 
 export default createProxy;
---
+++ src/utils/emitProxy.ts
@@ -0,0 +1,49 @@
+import type * as nx from "../types/Nexo.js";
+import isProxy from "./isProxy.js";
+import getProxyMap from "./getProxyMap.js";
+import { createDeferred, resolveWith } from "./deferred.js";
+import ProxyCreateEvent from "../events/ProxyCreateEvent.js";
+
+const emitProxy = (proxy: nx.Proxy): nx.Proxy => {
+  if (!isProxy(proxy)) return;
+
+  const deferred = createDeferred<nx.FunctionLike<[], nx.Proxy>>();
+  const proxyMap = getProxyMap();
+  const wrapper = proxyMap.get(proxy);
+
+  const event = new ProxyCreateEvent({
+    target: proxy,
+    data: {
+      id: wrapper.id,
+      target: wrapper.target,
+      result: deferred.promise,
+    },
+  });
+
+  // check whether the event got prevented
+  if (event.defaultPrevented) {
+    const { returnValue } = event;
+
+    if (isProxy(returnValue) && returnValue !== proxy) {
+      // get the new proxy wrapper
+      const returnedProxyWrapper = proxyMap.get(returnValue);
+      // revoke the original proxy in the event
+      wrapper.revoke();
+      // remove the original proxy from the maps
+      proxyMap.delete(proxy);
+      returnedProxyWrapper?.nexo?.entries.delete(wrapper.id);
+      // add or update the ID to the returned proxy
+      returnedProxyWrapper?.nexo?.entries.set(
+        proxyMap.get(returnValue).id,
+        new WeakRef(returnValue),
+      );
+
+      // return a different proxy object
+      return resolveWith(deferred.resolve, returnValue);
+    }
+  }
+
+  return resolveWith(deferred.resolve, proxy);
+};
+
+export default emitProxy;
