4d6e7c7b999c6cc60f6a5336761cde3e86116d41
Pedro Gallardo <pedrogallardo7011@gmail.com>
2026-08-17 02:42:40 +0000

    fix: type TraceableMap events with WeakRef values

--- src/types/events/TraceableMapEvents.ts
+++ src/types/events/TraceableMapEvents.ts
@@ -1,25 +1,25 @@
 declare global {
   namespace nx {
-    interface MapEvent<K, V> extends Event<Map<K, V>> {}
+    interface MapEvent<K, V extends object> extends Event<Map<K, WeakRef<V>>> {}
 
-    interface MapClearEvent<K, V> extends MapEvent<K, V> {}
+    interface MapClearEvent<K, V extends object> extends MapEvent<K, V> {}
 
-    interface MapDeleteEvent<K, V> extends MapEvent<K, V> {
+    interface MapDeleteEvent<K, V extends object> extends MapEvent<K, V> {
       data: {
         readonly key: K;
         readonly released: boolean;
       };
     }
 
-    interface MapSetEvent<K, V> extends MapEvent<K, V> {
+    interface MapSetEvent<K, V extends object> extends MapEvent<K, V> {
       data: {
         readonly key: K;
-        readonly value: V;
+        readonly value: WeakRef<V>;
       };
     }
 
     /** Map of available `traceable` map event names. */
-    type TraceableMapEvents<K, V> = {
+    type TraceableMapEvents<K, V extends object> = {
       set: (event: MapSetEvent<K, V>) => void;
       delete: (event: MapDeleteEvent<K, V>) => void;
       clear: (event: MapClearEvent<K, V>) => void;
